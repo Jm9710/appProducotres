4b6f65f18a952e2bdd5a03ae2f957cf14c003472
Jm9710 <jmoralesaltez@gmail.com>
2025-09-08 09:56:55 -0300

    CargarKml corregido

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1688,7 +1688,7 @@ useEffect(() => {
           style={{ backgroundColor: "rgba(0,0,0,0.5)" }}
           tabIndex="-1"
         >
-          <div className="modal-dialog modal">
+          <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title">Cargar Archivo KML</h5>
